4a47f1cb4e08d87d36ebaa1f7269d15cb3512d1f
Dave Wasmer <davewasmer@gmail.com>
2017-01-04 23:35:59 -0700

    feat: concat all eslint tests into a single file for faster execution

--- denali-build.js
+++ denali-build.js
@@ -1,6 +1,7 @@
 import { Builder } from 'denali';
 import Funnel from 'broccoli-funnel';
 import MergeTree from 'broccoli-merge-trees';
+import Concat from 'broccoli-concat';
 import LintTree from './lib/lint-tree';
 
 export default class DenaliEslintBuilder extends Builder {
@@ -11,6 +12,13 @@ export default class DenaliEslintBuilder extends Builder {
       if (this.project.environment === 'test') {
         let lintTestTree = new LintTree(tree, { generateTests: true, rootDir: dir });
         lintTestTree = new Funnel(lintTestTree, { destDir: 'test/lint' });
+        lintTestTree = new Concat(lintTestTree, {
+          outputFile: 'test/linting.js',
+          header: `import test from 'ava';`,
+          inputFiles: [ '**/*.lint-test.js' ],
+          sourceMapConfig: { enabled: true },
+          allowNone: true
+        });
         return new MergeTree([ lintTestTree, tree ]);
       }
       // Otherwise, just lint and move on
