205718c4b4629ef65add28f54da0bce672ee93b1
Dave Wasmer <davewasmer@gmail.com>
2018-01-30 14:03:49 -0700

    update to new builder api

--- denali-build.js
+++ denali-build.js
@@ -1,31 +1,32 @@
-const { Builder } = require('denali-cli');
+const { AddonBuilder } = require('denali-cli');
 const Funnel = require('broccoli-funnel');
 const Concat = require('broccoli-concat');
 const MergeTree = require('broccoli-merge-trees');
 
-module.exports = class DenaliEslintBuilder extends Builder {
+module.exports = class DenaliEslintBuilder extends AddonBuilder {
 
   processParent(tree, dir) {
+    return this.lint(tree);
+  }
+
+  lint(tree) {
     // Require here to avoid loading when we build this addon itself
     const LintTree = require('./lib/lint-tree').default;
-    if (this.project.lint) {
-      // If it's in test environment, generate test modules for each linted file
-      if (this.project.environment === 'test') {
-        let lintTestTree = new LintTree(tree, { generateTests: true, rootDir: dir });
-        lintTestTree = new Funnel(lintTestTree, { destDir: 'test/lint' });
-        lintTestTree = new Concat(lintTestTree, {
-          outputFile: 'test/linting.js',
-          header: `import test from 'ava';`,
-          inputFiles: [ '**/*.lint-test.js' ],
-          sourceMapConfig: { enabled: true },
-          allowNone: true
-        });
-        return new MergeTree([ lintTestTree, tree ]);
-      }
-      // Otherwise, just lint and move on
-      return new LintTree(tree, { rootDir: dir });
+    // If it's in test environment, generate test modules for each linted file
+    if (this.environment === 'test') {
+      let lintTestTree = new LintTree(tree, { generateTests: true, rootDir: dir });
+      lintTestTree = new Funnel(lintTestTree, { destDir: 'test/lint' });
+      lintTestTree = new Concat(lintTestTree, {
+        outputFile: 'test/linting.js',
+        header: `import test from 'ava';`,
+        inputFiles: [ '**/*.lint-test.js' ],
+        sourceMapConfig: { enabled: true },
+        allowNone: true
+      });
+      return new MergeTree([ lintTestTree, tree ]);
     }
-    return tree;
+    // Otherwise, just lint and move on
+    return new LintTree(tree, { rootDir: dir });
   }
 
 }
